2aed09ce080a0c4d8706c98434abc071d7badb83
yutaeho1 <yut02@naver.com>
2026-03-20 11:23:25 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main() {
+void print();
+int square(int n);
+
+int main(void) {
+    print();
+
+    int num = 5;
+    int result = square(num);
+    printf("%d %d\n", num, result);
 
     return 0;
+}
+
+void print() {
+    printf("Welcome to Computer Programming 03 Lab!\n");
+}
+
+int square(int n) {
+    return n * n;
 }
