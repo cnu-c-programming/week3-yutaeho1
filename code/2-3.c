60fab3cd45e4baa973b05906a042e5d7bb3df670
yutaeho1 <yut02@naver.com>
2026-03-20 12:11:21 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+void divide(int a, int b, int *q, int *r) {
+    *q = a/b;
+    *r = a%b;
+}
+
 int main() {
+    int a = 27;
+    int b = 4;
+    int q = 0;
+    int r = 0;
+
+    divide(a, b, &q, &r);
+    printf("%d\n%d\n", q, r);
 
     return 0;
 }
