b16718de8972bd8946b9bc6d64e08f9d59ab8c6c
yutaeho1 <yut02@naver.com>
2026-03-20 11:36:37 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void increment(int x) {
+    ++x;
+    printf("%d\n", x);
+}
+
 int main() {
+    int x = 10;
+
+    printf("%d\n", x);
+    increment(x);
+    printf("%d\n", x);
 
     return 0;
 }
