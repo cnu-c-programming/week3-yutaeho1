171eae40bf0b002b39b1fe93d37bbb04a7aed3f0
yutaeho1 <yut02@naver.com>
2026-03-20 11:27:37 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int power(int base, int exp) {
+    if(exp == 0){
+        return 1;
+    }
+    return base * power(base, exp-1);
+}
+
+int main(void) {
+    printf("%d\n", power(2,3));
+    printf("%d\n", power(5,0));
+    printf("%d\n", power(3,4));
 
     return 0;
 }
