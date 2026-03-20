020db1e1c010615b35a246e16895ea959fa99133
yutaeho1 <yut02@naver.com>
2026-03-20 12:02:16 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 int main() {
+    int x = 100, y = 200;
+
+    printf("x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("x = %d, y = %d\n", x, y);
 
     return 0;
 }
