0fb3e2c8664b7ee50a811edd27d7dfb28e4ed935
yutaeho1 <yut02@naver.com>
2026-03-20 12:15:47 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+int call_count() {
+    static int count = 0;
+    return ++count;
+}
+
 int main() {
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
+    printf("%d\n", call_count());
 
     return 0;
 }
