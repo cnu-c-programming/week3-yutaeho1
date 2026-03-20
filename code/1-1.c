21463822a75b87765746166d2ecf17ecf2f5d3c9
yutaeho1 <yut02@naver.com>
2026-03-20 11:19:18 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
-int main() {
+int add(int a, int b){
+    return a + b;
+}
+int sub(int a, int b){
+    return a - b;
+}
+int mul(int a, int b){
+    return a * b;
+}
+int div(int a, int b){
+    return a / b;
+}
 
-    return 0;
+int main(void) {
+    int a = 10, b = 5;
+
+    printf("%d + %d = %d\n", a, b, add(a, b));
+    printf("%d - %d = %d\n", a, b, sub(a, b));
+    printf("%d * %d = %d\n", a, b, mul(a, b));
+    printf("%d / %d = %d\n", a, b, div(a, b));
+
 }
